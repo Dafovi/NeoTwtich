51f8fc1bc5e1eb0618686fd19221092d80665b00
Dafovi <swan364@hotmail.com>
2026-06-26 10:02:46 -0500

    Split media library accessors

---
+++ NeoTwitch/MainWindow.MediaLibraryAccessors.cs
@@ -0,0 +1,66 @@
+using System.Collections.ObjectModel;
+using NeoTwitch.Models;
+using NeoTwitch.Services.Library;
+using NeoTwitch.ViewModels.Library;
+
+namespace NeoTwitch;
+
+public partial class MainWindow
+{
+    private ObservableCollection<MediaAssetConfig> GetMediaLibrary(MediaLibraryKind kind)
+    {
+        return kind == MediaLibraryKind.Image ? _config.ImageLibrary : _config.VideoLibrary;
+    }
+
+    private ObservableCollection<MediaGroupConfig> GetMediaGroups(MediaLibraryKind kind)
+    {
+        return kind == MediaLibraryKind.Image ? _config.ImageGroups : _config.VideoGroups;
+    }
+
+    private ObservableCollection<MediaLibraryRow> GetMediaRows(MediaLibraryKind kind)
+    {
+        return kind == MediaLibraryKind.Image ? _imageLibraryRows : _videoLibraryRows;
+    }
+
+    private ObservableCollection<MediaGroupRow> GetMediaGroupRows(MediaLibraryKind kind)
+    {
+        return kind == MediaLibraryKind.Image ? _imageGroupRows : _videoGroupRows;
+    }
+
+    private string GetMediaSearchText(MediaLibraryKind kind)
+    {
+        return kind == MediaLibraryKind.Image ? _imageSearchText : _videoSearchText;
+    }
+
+    private string GetMediaFilter(MediaLibraryKind kind)
+    {
+        return kind == MediaLibraryKind.Image ? _imageFilter : _videoFilter;
+    }
+
+    private string GetMediaGroupFilterId(MediaLibraryKind kind)
+    {
+        return kind == MediaLibraryKind.Image ? _imageGroupFilterId : _videoGroupFilterId;
+    }
+
+    private void SetMediaRefreshing(MediaLibraryKind kind, bool refreshing)
+    {
+        if (kind == MediaLibraryKind.Image)
+        {
+            _refreshingImageLibrary = refreshing;
+        }
+        else
+        {
+            _refreshingVideoLibrary = refreshing;
+        }
+    }
+
+    private string MediaLibraryTitle(MediaLibraryKind kind)
+    {
+        return _text.Get(MediaLibraryKindCatalog.Get(kind).TitleKey);
+    }
+
+    private void UpdateVideoVolumeText()
+    {
+        VideoVolumeValueText.Text = $"{_config.VideoVolumePercent}%";
+    }
+}

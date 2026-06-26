e4df01aa7fbe4fc71b837bd38c44cda5c49cbd58
Dafovi <swan364@hotmail.com>
2026-06-26 13:18:20 -0500

    Introduce application service composition

--- NeoTwitch/MainWindow.State.Services.cs
+++ NeoTwitch/MainWindow.State.Services.cs
@@ -1,28 +1,27 @@
 using NeoTwitch.Models;
 using NeoTwitch.Services;
-using NeoTwitch.Services.Activity;
-using NeoTwitch.Services.Alerts;
-using NeoTwitch.Services.Dashboard;
 using NeoTwitch.Services.Text;
 
 namespace NeoTwitch;
 
 public partial class MainWindow
 {
-    private readonly SettingsStore _settingsStore = new();
-    private readonly AudioPlayerService _audioPlayer = new();
-    private readonly SerialLightController _lightController = new();
-    private readonly TwitchAuthService _authService = new();
-    private readonly TwitchChatService _chatService = new();
-    private readonly AlexaRelayService _alexaRelayService = new();
-    private readonly ObsWebSocketService _obsService = new();
-    private readonly ObsOverlayService _obsOverlayService = new();
-    private readonly WindowsStartupService _windowsStartupService = new();
-    private readonly AppUpdateService _updateService = new();
-    private readonly IUiTextService _text = UiTextService.CreateDefault();
+    private readonly AppServices _services = AppServices.CreateDefault();
     private readonly AppStartupOptions _startupOptions;
     private readonly TwitchEventSubClient _eventSubClient;
-    private readonly ActivityLogService _activityLog = new();
-    private readonly DashboardSummaryService _dashboardSummary = new();
-    private readonly AlertQueueService _alertQueue = new();
+
+    private SettingsStore _settingsStore => _services.SettingsStore;
+    private AudioPlayerService _audioPlayer => _services.AudioPlayer;
+    private SerialLightController _lightController => _services.LightController;
+    private TwitchAuthService _authService => _services.AuthService;
+    private TwitchChatService _chatService => _services.ChatService;
+    private AlexaRelayService _alexaRelayService => _services.AlexaRelayService;
+    private ObsWebSocketService _obsService => _services.ObsService;
+    private ObsOverlayService _obsOverlayService => _services.ObsOverlayService;
+    private WindowsStartupService _windowsStartupService => _services.WindowsStartupService;
+    private AppUpdateService _updateService => _services.UpdateService;
+    private IUiTextService _text => _services.Text;
+    private Services.Activity.ActivityLogService _activityLog => _services.ActivityLog;
+    private Services.Dashboard.DashboardSummaryService _dashboardSummary => _services.DashboardSummary;
+    private Services.Alerts.AlertQueueService _alertQueue => _services.AlertQueue;
 }
---
+++ NeoTwitch/Services/AppServices.cs
@@ -0,0 +1,88 @@
+using NeoTwitch.Services.Activity;
+using NeoTwitch.Services.Alerts;
+using NeoTwitch.Services.Dashboard;
+using NeoTwitch.Services.Text;
+
+namespace NeoTwitch.Services;
+
+public sealed class AppServices
+{
+    public AppServices(
+        SettingsStore settingsStore,
+        AudioPlayerService audioPlayer,
+        SerialLightController lightController,
+        TwitchAuthService authService,
+        TwitchChatService chatService,
+        AlexaRelayService alexaRelayService,
+        ObsWebSocketService obsService,
+        ObsOverlayService obsOverlayService,
+        WindowsStartupService windowsStartupService,
+        AppUpdateService updateService,
+        IUiTextService text,
+        ActivityLogService activityLog,
+        DashboardSummaryService dashboardSummary,
+        AlertQueueService alertQueue)
+    {
+        SettingsStore = settingsStore;
+        AudioPlayer = audioPlayer;
+        LightController = lightController;
+        AuthService = authService;
+        ChatService = chatService;
+        AlexaRelayService = alexaRelayService;
+        ObsService = obsService;
+        ObsOverlayService = obsOverlayService;
+        WindowsStartupService = windowsStartupService;
+        UpdateService = updateService;
+        Text = text;
+        ActivityLog = activityLog;
+        DashboardSummary = dashboardSummary;
+        AlertQueue = alertQueue;
+    }
+
+    public SettingsStore SettingsStore { get; }
+
+    public AudioPlayerService AudioPlayer { get; }
+
+    public SerialLightController LightController { get; }
+
+    public TwitchAuthService AuthService { get; }
+
+    public TwitchChatService ChatService { get; }
+
+    public AlexaRelayService AlexaRelayService { get; }
+
+    public ObsWebSocketService ObsService { get; }
+
+    public ObsOverlayService ObsOverlayService { get; }
+
+    public WindowsStartupService WindowsStartupService { get; }
+
+    public AppUpdateService UpdateService { get; }
+
+    public IUiTextService Text { get; }
+
+    public ActivityLogService ActivityLog { get; }
+
+    public DashboardSummaryService DashboardSummary { get; }
+
+    public AlertQueueService AlertQueue { get; }
+
+    public static AppServices CreateDefault()
+    {
+        return new AppServices(
+            new SettingsStore(),
+            new AudioPlayerService(),
+            new SerialLightController(),
+            new TwitchAuthService(),
+            new TwitchChatService(),
+            new AlexaRelayService(),
+            new ObsWebSocketService(),
+            new ObsOverlayService(),
+            new WindowsStartupService(),
+            new AppUpdateService(),
+            UiTextService.CreateDefault(),
+            new ActivityLogService(),
+            new DashboardSummaryService(),
+            new AlertQueueService());
+    }
+}
